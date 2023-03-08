6a2bb0ea328645cf6b9ac3cf80caa7d442796bc7
Wissam Mansour <wissman77@gmail.com>
2023-03-08 12:01:25 +0200

    rename to driver

---
+++ driver.js
@@ -0,0 +1,6 @@
+const prettyPrint = require('./prettyPrint');
+const Tree = require('./tree');
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+prettyPrint(tree.root);
