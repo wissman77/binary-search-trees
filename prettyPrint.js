62a7f5d11b4949b073cdfeee0a382c83aade3b78
Wissam Mansour <wissman77@gmail.com>
2023-03-08 11:14:44 +0200

    create prettyPrint function

---
+++ prettyPrint.js
@@ -0,0 +1,11 @@
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+module.exports = prettyPrint;
